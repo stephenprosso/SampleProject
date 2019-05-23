01e1a5fddb7a673e004fe37aecbaa871b570b6de
Stephen Rosso <stephenprosso@gmail.com>
2019-05-23 09:06:00 -0700

    added application.js and references in site.master and bundleconfig.js

--- SampleProject/Site.Master.designer.cs
+++ SampleProject/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace SampleProject {
     public partial class SiteMaster {
         
         /// <summary>
+        /// nothingToSeeHere control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField nothingToSeeHere;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>

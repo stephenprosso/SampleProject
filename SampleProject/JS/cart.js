1ee62f087609e895936ebdfa48971843394eeab1
Stephen Rosso <stephenprosso@gmail.com>
2019-06-28 13:31:03 -0700

    cart

--- SampleProject/JS/cart.js
+++ SampleProject/JS/cart.js
@@ -162,7 +162,12 @@
 
 
             $("#directionalDisplayDiv").css('display', 'block');
+            $("#userFieldDiv").css('display', 'block');
+
+            $("#infoBox").css('display', 'block');
             $("#partDetailsAndLocationDiv").css('display', 'block');
+            $("#partDetailsAndLocationDiv1").css('display', 'block');
+
             $("#userFieldQuantity").css('display', 'block');
             $("#BatchDataDiv").css('display', 'block');
 
@@ -209,6 +214,7 @@
 
             if (action === "Location Validation" && subAction === "Prompt") {
                 $("#id01").css('display', 'none');
+
                 $("#validateLocationDiv").css('display', 'block');
                 document.getElementById("MainContent_fullToteButton").disabled = true;
                 document.getElementById("MainContent_taskCompleteButton").disabled = true;
@@ -219,6 +225,7 @@
             }
             if (action === "Part Validation" && subAction === "Prompt") {
                 $("#id01").css('display', 'none');
+
                 document.getElementById("MainContent_fullToteButton").disabled = true;
                 document.getElementById("MainContent_taskCompleteButton").disabled = true;
                 $("#validatePartDiv").css('display', 'block');
@@ -242,14 +249,57 @@
                 //hide the task complete button
             }
 
-            if (action === "Batch Complete" && subAction === "Display") {
-                $("#LoginDiv").css('display', 'none');
-
-                $("#batchCompleteTitle").css('display', 'block');
-                $("#BatchCompleteDiv").css('display', 'block');
-
-            }
         }
+        if (action === "Batch Complete" && subAction === "Display") {
+
+            $("#LoginDiv").css('display', 'none');
+
+            $("#directionalDisplayDiv").css('display', 'none');
+            $("#partDetailsAndLocationDiv").css('display', 'none');
+            $("#userFieldQuantity").css('display', 'none');
+            $("#BatchDataDiv").css('display', 'block');
+
+            $("#button-div").css('display', 'none');
+
+            $("#batchCompleteTitle").css('display', 'block');
+            $("#BatchCompleteDiv").css('display', 'block');
+
+        }
+
+        if (action === "Early Exit" && subAction === "Display") {
+            $("#loginDiv").css('display', 'none');
+            $("#CartPickingTitle").css('display', 'none');
+
+            $("#button-div").css('display', 'none');
+            $("#earlyExitTitle").css('display', 'block');
+            $("#earlyExitButtons").css('display', 'block');
+        }
+
+        //if the same fields from lpn Validation i.e. Display data, batch data then full tote can be moved up with
+        //the other if statements
+
+        if (action === "Full Tote" && subAction === "Prompt Full Tote") {
+            $("#button-div").css('display', 'none');
+            $("#BatchDataDiv").css('display', 'none');
+            $("#promptFullToteDiv").css('display', 'block');
+            document.getElementById('MainContent_promptFullToteTB').focus();
+        }
+        if (action === "Full Tote" && subAction === "Prompt Current Qty") {
+            $("#button-div").css('display', 'none')
+            $("#BatchDataDiv").css('display', 'none');
+            $("#promptCurrentQtyDiv").css('display', 'block');
+            $("#promptFullToteDiv").css('display', 'none');
+            document.getElementById('MainContent_promptCurrentQtyTB').focus();
+        }
+        if (action === "Full Tote" && subAction === "Prompt New Tote") {
+            $("#button-div").css('display', 'none')
+            $("#BatchDataDiv").css('display', 'none');
+            $("#promptCurrentQtyDiv").css('display', 'none');
+            $("#promptFullToteDiv").css('display', 'none');
+            $("#promptNewToteDiv").css('display', 'block');
+            document.getElementById('MainContent_promptNewToteTB').focus();
+        }
+
     };
     ws.onopen = function () {
         // alert("Web Socket Open");
@@ -260,127 +310,4 @@
 
     };
 
-};
-//functions for buttons
-function sendLogin() {
-
-    var UserID = document.getElementById('MainContent_UserID').value;
-    var Password = document.getElementById('MainContent_PWD').value;
-    var cart = new URLSearchParams(window.location.search).get("cart");
-    var ntsh = document.getElementById('nothingToSeeHere').value;
-
-    var data = JSON.stringify({ "action": "Login", "SubAction": "Initiate", "Cart": cart, "UserResponse1": UserID, "UserResponse2": Password, "UserResponse3": ntsh });
-    console.log(data);
-    console.log("send data");
-    ws.send(data);
-    console.log("data sent");
-}
-
-function sendLogout() {
-
-    var cart = new URLSearchParams(window.location.search).get("cart");
-
-    var data = JSON.stringify({ "action": "Zone Group", "SubAction": "LogOut", "Cart": cart });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
-function clearError() {
-
-    $("#errorDiv").css('display', 'none');
-    $("#connectedDiv").css('display', 'block');
-
-}
-
-function sendSelectedZoneGrouping() {
-
-    var selectedZoneGrouping = "1";
-    var cart = new URLSearchParams(window.location.search).get("cart");
-    alert("WTF");
-    var data = JSON.stringify({ "Action": "Zone Group", "SubAction": "Response", "Cart": cart, "UserResponse1": selectedZoneGrouping });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
-
-function sendSelectedBucket(selectedBucket) {
-
-
-    var cart = new URLSearchParams(window.location.search).get("cart");
-    var data = JSON.stringify({ "Action": "Work Dashboard", "SubAction": "Selected Bucket", "Cart": cart, "UserResponse1": selectedBucket });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
-
-function sendLPN() {
-
-    var LPN = document.getElementById('MainContent_LPNTB').value;
-    var cart = new URLSearchParams(window.location.search).get("cart");
-    var data = JSON.stringify({ "Action": "Batch Setup", "SubAction": "LPN Scanned", "Cart": cart, "UserResponse1": LPN });
-    console.log(data);
-    console.log("send data");
-    ws.send(data);
-    console.log("data sent");
-}
-
-function processBatch() {
-
-    var cart = new URLSearchParams(window.location.search).get("cart");
-    var data = JSON.stringify({ "Action": "Batch Setup", "SubAction": "Process Batch", "Cart": cart });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
-
-function sendLocnEmpty() {
-
-    var emptyLocn = prompt("Scan Empty Location");
-    var data = JSON.stringify({ "Action": "Notify Host Locn Empty", "SubAction": "Request", "Cart": cart, "UserResponse1": emptyLocn });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-
-
-}
-
-function sendStartAisle() {
-
-    var aisle = document.getElementById('MainContent_startAisleTB').value;
-    var cart = new URLSearchParams(window.location.search).get("cart");
-
-    var data = JSON.stringify({ "Action": "Start Aisle", "SubAction": "Response", "Cart": cart, "UserResponse1": aisle });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
-
-
-function sendBack() {
-
-    var cart = new URLSearchParams(window.location.search).get("cart");
-
-    var data = JSON.stringify({ "Action": "Batch Setup", "SubAction": "Clear Cart", "Cart": cart });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-
-}
-
-function sendRetryLights() {
-    var cart = new URLSearchParams(window.location.search).get("cart");
-
-    var data = JSON.stringify({ "Action": "Batch Complete", "SubAction": "Retry Lights", "Cart": cart });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-
-}
-function sendTaskComplete() {
-    var cart = new URLSearchParams(window.location.search).get("cart");
-
-    var data = JSON.stringify({ "Action": "Batch Complete", "SubAction": "Task Complete", "Cart": cart });
-    console.log(data);
-    ws.send(data);
-    console.log("data sent");
-}
+}; 

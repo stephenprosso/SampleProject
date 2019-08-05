1872d2e9890359f87c23cef43abd5b43c6ca89c2
Stephen Rosso <stephenprosso@gmail.com>
2019-08-05 07:37:03 -0700

    Broken page, but documenting the code

--- SampleProject/ClassOfFunctionsThatCanBeCalledFromOtherPages.cs
+++ SampleProject/ClassOfFunctionsThatCanBeCalledFromOtherPages.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 
 namespace SampleProject
 {
     public class ClassOfFunctionsThatCanBeCalledFromOtherPages
     {
+        public static async Task<String> RemoveToteTask(string cartId, string toteNumber)
+        {
+            string responseData = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"http://{HttpContext.Current.Request.Url.Host}/");
+                //add using statements 
+                StringContent content = new StringContent($"APPNAME=PoweredPIKWebServer&PRGNAME=<PRM>&ARGUMENTS=-A{cartId}{','}" + "-A" + toteNumber.ToUpper());
+                // HTTP POST
+                HttpResponseMessage response = await client.PostAsync(GlobalVariables.MagicVersion, content).ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
+                {
+                    responseData = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                }
+                else
+                {
+                    responseData = "Failure Callout to ISAPI returned " + response.StatusCode;
+                }
+            }
+            return responseData;
+        }
+
     }
 }

284571e624b5e0ca172ed89d3e6292d55a25ada7
Stephen Rosso <stephenprosso@gmail.com>
2019-08-06 08:52:17 -0700

    comment out functions to the code doesnt crash

--- SampleProject/ClassOfFunctionsThatCanBeCalledFromOtherPages.cs
+++ SampleProject/ClassOfFunctionsThatCanBeCalledFromOtherPages.cs
@@ -8,26 +8,26 @@ namespace SampleProject
 {
     public class ClassOfFunctionsThatCanBeCalledFromOtherPages
     {
-        public static async Task<String> RemoveToteTask(string cartId, string toteNumber)
-        {
-            string responseData = "";
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri($"http://{HttpContext.Current.Request.Url.Host}/");
-                //add using statements 
-                StringContent content = new StringContent($"APPNAME=PoweredPIKWebServer&PRGNAME=<PRM>&ARGUMENTS=-A{cartId}{','}" + "-A" + toteNumber.ToUpper());
-                // HTTP POST
-                HttpResponseMessage response = await client.PostAsync(GlobalVariables.MagicVersion, content).ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
-                {
-                    responseData = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                }
-                else
-                {
-                    responseData = "Failure Callout to ISAPI returned " + response.StatusCode;
-                }
-            }
-            return responseData;
-        }
+        //public static async Task<String> RemoveToteTask(string cartId, string toteNumber)
+        //{
+        //    string responseData = "";
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri($"http://{HttpContext.Current.Request.Url.Host}/");
+        //        //add using statements 
+        //        StringContent content = new StringContent($"APPNAME=PoweredPIKWebServer&PRGNAME=<PRM>&ARGUMENTS=-A{cartId}{','}" + "-A" + toteNumber.ToUpper());
+        //        // HTTP POST
+        //        HttpResponseMessage response = await client.PostAsync(GlobalVariables.MagicVersion, content).ConfigureAwait(false);
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            responseData = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        //        }
+        //        else
+        //        {
+        //            responseData = "Failure Callout to ISAPI returned " + response.StatusCode;
+        //        }
+        //    }
+        //    return responseData;
+        //}
     }
 }
